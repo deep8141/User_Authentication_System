a1884b21b8b0d0b305d8f447b47c32b4b64a2916
fullstackmspl <fullstack.mspl@gmail.com>
2024-05-27 10:21:55 +0530

    new code

---
+++ src/mataData.js
@@ -0,0 +1,15 @@
+const routes = [
+    { 
+      path: "/", 
+      title: "Home", 
+      description: "Welcome to our home page." 
+    },
+    { 
+      path: "/profile", 
+      title: "Profile", 
+      description: "Welcome to our Profile page." 
+    },
+  ];
+  
+  export default routes;
+  
